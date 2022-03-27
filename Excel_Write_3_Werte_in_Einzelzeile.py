7f44f385480ab3f99cd94196b26c1d2fa2bf44ec
pyrrhus-ich <menzelfrank@gmail.com>
2022-03-27 11:43:46 +0200

    write function for 3

--- Excel_Write_3_Werte_in_Einzelzeile.py
+++ Excel_Write_3_Werte_in_Einzelzeile.py
@@ -1,26 +1,24 @@
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 
-wb = Workbook()
-ws = wb.active
 myList=['Frank','hatte','gestern','Geburtstag']
-spalten=[]
-    #  befüllt die Liste spalten mit der benötigten Anzahl Buchstaben
-for x in list(range(1,len(myList)+1)):
-    spaltenName = get_column_letter(x)
-    spalten.append(spaltenName)
-    # schreibt die Werte aus myList in das Excel. Wobei die Zeile 
-    # immer gleich bleibt 'zeilenNummer' und nur der Spaltenbuch-
-    # stabe sich ändert
-for el in spalten:
-    i=0
-    zeilenNummer = "1"
-    while i < len(spalten):
-        ws[spalten[i]+zeilenNummer]=myList[i]
-        i+=1
-
-wb.save("Bsp_Excel_Write_3.xlsx")
 
 
+def writeInOneLine(listName, excelFileName):
+    wb = Workbook()
+    ws = wb.active
+    spalten=[]
+    for x in list(range(1,len(myList)+1)):
+        spaltenName = get_column_letter(x)
+        spalten.append(spaltenName)
+    for el in spalten:
+        i=0
+        zeilenNummer = "1"
+        while i < len(spalten):
+            ws[spalten[i]+zeilenNummer]=myList[i]
+            i+=1
+    wb.save(excelFileName)
+
+writeInOneLine(myList,"Bsp_Excel_Write_3.xlsx")
 
 
