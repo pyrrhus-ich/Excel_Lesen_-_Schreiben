ccc6de4862176ea9a6d346accfe6031c5b90dda2
pyrrhus-ich <menzelfrank@gmail.com>
2022-03-27 11:32:24 +0200

    Teil 3 abgeschlossen

--- Excel_Write_3_Werte_in_Einzelzeile.py
+++ Excel_Write_3_Werte_in_Einzelzeile.py
@@ -4,10 +4,23 @@ from openpyxl.utils import get_column_letter
 wb = Workbook()
 ws = wb.active
 myList=['Frank','hatte','gestern','Geburtstag']
-
+spalten=[]
+    #  befüllt die Liste spalten mit der benötigten Anzahl Buchstaben
 for x in list(range(1,len(myList)+1)):
-    ch = get_column_letter(x)
-    print(ch)
+    spaltenName = get_column_letter(x)
+    spalten.append(spaltenName)
+    # schreibt die Werte aus myList in das Excel. Wobei die Zeile 
+    # immer gleich bleibt 'zeilenNummer' und nur der Spaltenbuch-
+    # stabe sich ändert
+for el in spalten:
+    i=0
+    zeilenNummer = "1"
+    while i < len(spalten):
+        ws[spalten[i]+zeilenNummer]=myList[i]
+        i+=1
+
+wb.save("Bsp_Excel_Write_3.xlsx")
+
 
 
 
